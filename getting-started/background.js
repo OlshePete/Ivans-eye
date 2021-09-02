0bef3e48ce682015184a2364582ab8f2beff2d88
Olshevskii Peter <olshevskii.pv@gmail.com>
2021-09-02 15:21:03 +0300

    check rerender

--- getting-started/background.js
+++ getting-started/background.js
@@ -1,42 +1,54 @@
 let online = false;
-const whiteList=['stackoverflow.com', 'learn.javascript.ru'];
-const searcherList = ['yandex.ru', 'www.google.ru','google.ru', 'www.google.com'];
+const whiteList = ['stackoverflow.com', 'learn.javascript.ru'];
+const searcherList = ['yandex', 'google'];
+
 
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.local.set({'online': online });
+  chrome.storage.local.set({ 'online': online });
   console.log('Статус:', online);
 });
 
 function parseUrl(tabURL) {
-  const url = new URL(tabURL);  
+  const url = new URL(tabURL);
   const regexp = /(?<=text\=|q\=)([^\&]*)/;
-  const isSearcher= searcherList.includes(url.host);
-  const isWhite= whiteList.includes(url.host);
-  const text=regexp.exec(url.search);
-  //  яндекс меняет формат URL обновить regexp
-// если запрос пустой? или вкладка только открылась
+  const hostname = url.hostname.replace(/.+\/\/|www.|\..+/g, '');
+  const isSearcher = searcherList.includes(hostname);
+
+  const isWhite = whiteList.includes(url.hostname);
+
+  const text = regexp.exec(url.search);
+  // яндекс меняет формат URL обновить regexp
+  // если использует домен поисковика но не для поиска
   if (isSearcher) {
-    // console.log("Запрос от пользователя в поисковик")
-    console.log("Отправляем: ", text[0])
+    const status = url.search.includes(`suggest_req`)
+    if (!status) {
+      console.log("Запрос в поисковик", hostname);
+      console.log("Данные для отправки: ", `${hostname} ${text[0].replace(/\+/g, " ")}`);
+    }
   } else if (isWhite) {
-      console.log("Запрос страницы из белого списка")
-      console.log("Отправляем: ", tabURL)    
+    console.log("Переход на страницу из белого списка")
+    console.log("Данные для отправки: ", `${tabURL}`)
   } else return
 }
 
 function sendUrl() {
-  chrome.tabs.query({'active': true}, function(tabs) {
+  chrome.tabs.query({ 'active': true }, function (tabs) {
     var tabURL = tabs[0].url;
-    // console.log(tabURL);
-    parseUrl(tabURL)
+    chrome.storage.local.get(['online'], function (result) {
+      online = result.online
+      if (online) parseUrl(tabURL);
+    })
   })
+
 }
 
-chrome.tabs.onActivated.addListener((activeInfo) => {  
-  sendUrl();
+chrome.tabs.onActivated.addListener((activeInfo) => {
+  sendUrl(activeInfo);
 })
 
-chrome.tabs.onUpdated.addListener((activeInfo) => {  
-  sendUrl();
+chrome.tabs.onUpdated.addListener((activeInfo, change) => {
+  if (change.url && change.status && change.status === "loading") {
+    sendUrl(activeInfo);
+  }
 })
 
